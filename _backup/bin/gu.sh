db6f92bd319205a923a9f8b84cbd64cafc73f4c7
saibakira <saibakira@tutanota.com>
2023-04-16 01:30:42 +0200

    backup

---
+++ _backup/bin/gu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp /home/panku/bin/* /home/panku/websites/insanity/_backup/bin
+
+git -C /home/panku/websites/insanity status
+git -C /home/panku/websites/insanity add -A
+git -C /home/panku/websites/insanity commit -m "backup"
+git -C /home/panku/websites/insanity push
