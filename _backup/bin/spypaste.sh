42aa951b64e38816e08193cbca0492f3331a0ac6
saibakira <saibakira@tutanota.com>
2023-04-16 01:12:43 +0200

    backup

--- _backup/bin/spypaste.sh
+++ _backup/bin/spypaste.sh
@@ -3,6 +3,7 @@
 # Banner
 
 HANG=1
+SPY_DIR=~/spypaste
 
 while :; do
     printf "                                    \n"
@@ -14,14 +15,14 @@ while :; do
     LINKS="$(grep -R -h -o "https://www.oetec.com/pastebin/.*[^\s]" ~/irclogs/ | cut -d ' ' -f1)"
 
     for PASTE in $(echo "$LINKS" | rev | cut -d '/' -f1 | rev); do
-        if [ ! -f ~/spypaste/$PASTE ]; then
+        if [ ! -f $SPY_DIR/"$PASTE" ]; then
             if curl -s -I "https://www.oetec.com/pastebin/plain/$PASTE" | grep "Expires: " 1>/dev/null; then
                 printf "\nDownloading: %s\n" "$PASTE"
-                wget "https://www.oetec.com/pastebin/plain/$PASTE" -O ~/spypaste/$PASTE -q --show-progress
+                wget "https://www.oetec.com/pastebin/plain/$PASTE" -O $SPY_DIR/"$PASTE" -q --show-progress
                 echo
             else
                 printf "%s expired.\n" "$PASTE"
-                echo "expired" > ~/spypaste/$PASTE
+                echo "expired" > $SPY_DIR/"$PASTE"
             fi
         fi
     done
