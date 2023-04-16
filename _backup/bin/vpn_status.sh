8f34fd0e7243b4a28753f4ecf795b12719b023f5
saibakira <saibakira@tutanota.com>
2023-04-16 18:05:49 +0200

    backup

--- _backup/bin/vpn_status.sh
+++ _backup/bin/vpn_status.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if ip link show proton0 2>/dev//null 1>/dev/null; then
-    printf "VPN is ON: %s" "$(curl -s ifconfig.me)"
+    if IP="$(curl -s --connect-timeout 1 ifconfig.me)"; then
+        printf "VPN is ON: %s" "$IP"
+    else
+        printf "Network error."
+    fi
 else
     printf "VPN is OFF"
 fi
