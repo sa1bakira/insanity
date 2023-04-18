53edded5c753802054925c09ac99feab1b6cb8ed
saibakira <saibakira@tutanota.com>
2023-04-18 04:46:43 +0200

    backup

--- _backup/bin/autostart.sh
+++ _backup/bin/autostart.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 #irssi window
-xterm -geom 102x39+30+26 -e 'irssi' &
+xterm -geom 102x30+30+26 -e 'irssi' &
 
 #vertical leap
 xterm -fg darkgreen -geom 1x65+8+25 -e 'tput civis; watch -n 0.1 -t ~/bin/vertical_leap.sh' &
@@ -25,10 +25,10 @@ xterm -fg grey -geom 84x4+1230+1000 -e 'tput civis; watch -n 0.1 -t ~/bin/insani
 xterm -fg yellow -geom 44x4+520+1000 -e 'tput civis; watch -n 5 -t ~/bin/genunix.sh' &
 
 # utility
-xterm -geom 102x20+30+660 -e '~/bin/banner.sh; bash' &
+xterm -geom 102x29+30+519 -e '~/bin/banner.sh; bash' &
 
 # coding
-xterm -geom 130x59+860+33 -e '~/bin/banner.sh; bash' &
+xterm -geom 130x60+860+25 -e '~/bin/banner.sh; bash' &
 
 # spypaste
 xterm -fg green -geom 35x4+910+1000 -e 'tput civis; watch -n 1 -t ~/bin/spypaste.sh' &
