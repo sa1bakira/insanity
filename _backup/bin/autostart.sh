857a6816138efa392bf8935447f3498cc71b7c67
saibakira <saibakira@tutanota.com>
2023-04-18 19:47:39 +0200

    backup

--- _backup/bin/autostart.sh
+++ _backup/bin/autostart.sh
@@ -2,7 +2,7 @@
 
 # LXDE windows autostart
 
-#vertical leap
+#vertical leap left
 xterm -fg green -geom 2x66+3+8 -e 'tput civis; watch -n 0.1 -t ~/bin/vertical_leap.sh' &
 
 #irssi
@@ -27,7 +27,7 @@ xterm -fg yellow -geom 30x1+262+1050 -e 'tput civis; watch -n 1 -t ~/bin/vpn_sta
 xterm -fg yellow -geom 44x4+520+1000 -e 'tput civis; watch -n 5 -t ~/bin/genunix.sh' &
 
 # coding
-xterm -geom 130x61+860+8 -e 'cat ~/bin/banner.txt; bash' &
+xterm -geom 128x61+856+8 -e 'cat ~/bin/banner.txt; bash' &
 
 # spypaste
 xterm -fg green -geom 35x4+900+1000 -e 'tput civis; watch -n 1 -t ~/bin/spypaste.sh' &
@@ -37,3 +37,6 @@ xterm -fg cyan -geom 42x4+1220+1000 -e 'tput civis; watch -n 0.1 -t ~/bin/inject
 
 # insanity
 xterm -fg grey -geom 42x4+1555+1000 -e 'tput civis; watch -n 0.1 -t ~/bin/insanity.sh' &
+
+#vertical leap right
+xterm -fg green -geom 2x66+1895+8 -e 'tput civis; watch -n 0.1 -t ~/bin/vertical_leap.sh' &
