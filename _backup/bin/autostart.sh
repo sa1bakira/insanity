459a5072fe75ba316798d17314bbe9eb379f9b1a
sa1bakira <saibakira@tutanota.com>
2023-04-20 23:52:31 +0200

    backup

--- _backup/bin/autostart.sh
+++ _backup/bin/autostart.sh
@@ -3,7 +3,7 @@
 # LXDE windows autostart
 
 #vertical leap left
-xterm -fg green -geom 2x66+3+8 -e 'tput civis; watch -x -n 0.1 -t ~/bin/vertical_leap.sh' &
+#xterm -fg green -geom 2x66+3+8 -e 'tput civis; watch -x -n 0.1 -t ~/bin/vertical_leap.sh' &
 
 #irssi
 xterm -geom 102x31+30+8 -e 'irssi' &
@@ -18,19 +18,22 @@ xterm -fg red -geom 27x2+30+1000 -e 'tput civis; watch -x -n 1 -t ~/bin/uptime.s
 xterm -fg green -geom 27x1+30+1050 -e 'tput civis; watch -x -n 1 -t ~/bin/icmp.sh' &
 
 # widget
-xterm -fg cyan -geom 30x2+262+1000 -e 'tput civis; watch -x -n 1 -t ~/bin/widget.sh' &
+#xterm -fg cyan -geom 30x2+262+1000 -e 'tput civis; watch -x -n 1 -t ~/bin/widget.sh' &
 
 # vpn
-xterm -fg yellow -geom 30x1+262+1050 -e 'tput civis; watch -x -n 1 -t ~/bin/vpn_status.sh' &
+#xterm -fg yellow -geom 30x1+262+1050 -e 'tput civis; watch -x -n 1 -t ~/bin/vpn_status.sh' &
 
 # genunix
-xterm -fg yellow -geom 44x4+520+1000 -e 'tput civis; watch -x -n 5 -t ~/bin/genunix.sh' &
+xterm -fg yellow -geom 44x4+690+1000 -e 'tput civis; watch -x -n 5 -t ~/bin/genunix.sh' &
 
 # coding
 xterm -geom 128x61+856+8 -e 'cat ~/bin/banner.txt; bash' &
 
+# mpv
+xterm -fg red -geom 50x4+270+1000 -e 'bash '
+
 # spypaste
-xterm -fg green -geom 35x4+900+1000 -e 'tput civis; watch -x -n 1 -t ~/bin/spypaste.sh' &
+xterm -fg green -geom 35x4+1060+1000 -e 'tput civis; watch -x -n 1 -t ~/bin/spypaste.sh' &
 
 # inject
 xterm -fg cyan -geom 40x4+1215+1000 -e 'tput civis; watch -x -n 0.1 -t ~/bin/inject.sh' &
@@ -39,4 +42,4 @@ xterm -fg cyan -geom 40x4+1215+1000 -e 'tput civis; watch -x -n 0.1 -t ~/bin/inj
 xterm -fg grey -geom 40x4+1555+1000 -e 'tput civis; watch -x -n 0.1 -t ~/bin/insanity.sh' &
 
 #vertical leap right
-xterm -fg green -geom 2x66+1895+8 -e 'tput civis; watch -x -n 0.1 -t ~/bin/vertical_leap.sh' &
+#xterm -fg green -geom 2x66+1895+8 -e 'tput civis; watch -x -n 0.1 -t ~/bin/vertical_leap.sh' &
--- _backup/bin/spypaste.sh
+++ _backup/bin/spypaste.sh
@@ -15,7 +15,10 @@ for PASTE in $(echo "$LINKS" | rev | cut -d '/' -f1 | rev); do
     if [ ! -f $SPY_DIR/"$PASTE" ]; then
         if curl -s -I "https://www.oetec.com/pastebin/plain/$PASTE" | grep "Expires: " 1>/dev/null; then
             printf "      Downloading: %s            \n" "$PASTE"
-            wget "https://www.oetec.com/pastebin/plain/$PASTE" -O $SPY_DIR/"$PASTE" -q 1>/dev/null
+            curl -s --connect-timeout 5 \ 
+                 --max-time 60 \
+                 -L "https://www.oetec.com/pastebin/plain/$PASTE" \
+                 -o $SPY_DIR/"$PASTE" -q 1>/dev/null
         else
             printf "        %s expired.                \n" "$PASTE"
             echo "expired" > $SPY_DIR/"$PASTE"
