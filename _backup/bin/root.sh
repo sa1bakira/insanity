eff08c35b1bf942e194c238d3a83f60ef0e4ed02
sa1bakira <saibakira@tutanota.com>
2023-04-21 10:42:57 +0200

    backup

--- _backup/bin/root.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xterm -bg darkred -fg white -fs 12 -geometry 60x20 -e 'su'
--- _backup/bin/vertical_leap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-cat /dev/random
-
-#while :; do
-    tr -dc A-Za-z0-9 </dev/urandom | head -c 160; echo ''
-#done
